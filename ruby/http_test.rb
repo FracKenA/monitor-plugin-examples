23b2e167ba168fab1432ed5e24f5ae4923c7974b
Ryan Quinn <rquinn@op5.com>
2018-09-07 17:20:52 +0000

    Updated to HTTP connection.
     On branch master
     Changes to be committed:
    	modified:   http_test.rb

--- ruby/http_test.rb
+++ ruby/http_test.rb
@@ -8,15 +8,16 @@ require 'resolv-replace'
 data = {"host_name": "dt-x00a","service_description": "File Check Passive","status_code": "0","plugin_output": "Everything is fine."}
 url = URI("https://khan.op5.com/api/command/PROCESS_SERVICE_CHECK_RESULT?format=json")
 
+# 0 to disable SSL verification, 1 to enable
+verify = 0
+
 request = Net::HTTP::Post.new(url)
-request.basic_auth("user", "pass")
+request.basic_auth("user", "password")
 request["content-type"] = 'application/json'
-request["authorization"] = 'Basic ZHR1cDpnPmQtOEt+Mys4RVU/'
 request.body = data.to_json
 
-http = Net::HTTP.new(url.host, url.port)
-http.use_ssl = true
-http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+response = Net::HTTP.start(url.host, url.port, :use_ssl => true, :verify_mode => verify) {|http|
+  http.request(request)
+}
 
-response = http.request(request)
 puts response.read_body

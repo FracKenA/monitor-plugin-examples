9908fddf14c77f33c3e04904865d1109e923a42a
Ryan Quinn <rquinn@op5.com>
2018-09-21 16:41:18 +0000

    Return code lab completed.
     Changes to be committed:
    	modified:   file_check-local-stateless.py

--- python3/file_check-local-stateless.py
+++ python3/file_check-local-stateless.py
@@ -1,7 +1,22 @@
 #!/usr/bin/env python3
 
+import argparse
+
+
 def main():
-    return 0
+    ret_val = 0
+
+    with open('sample_file') as file_source:
+        data = file_source.read().rstrip()
+
+    data = int(data)
+
+    if data > 20:
+        ret_val = 2
+    elif data > 10:
+        ret_val = 1
+
+    return ret_val
 
 
 if __name__ == "__main__":
